288518b4e27eb97fc79f5a4d68ffabdd3e3eb435
Flur <Flur@nxt.ru>
2015-01-27 15:37:55 +0200

    Work is in progress

--- www/script.js
+++ www/script.js
@@ -1,146 +1,16 @@
-//(function() { 
-
-var url = 'click.mp3',      
-    audioContext = null,
-    isPlaying = false,
-    current4Note, 
-    tempo = 60 / 120,
-    lookahead = 1000,
-    nextNoteTime = 0,
-    scheduleAheadTime = 0.1,  // не могу еще толком понять вот эту штуку
-    timerId,
-    loadClick,
-    timerId,
-    source,
-    click;
-
 document.addEventListener('DOMContentLoaded', init);
+var playButton = document.getElementById("play"),
+    bpm = document.getElementById('bpm'),
+    range = document.getElementById('range'),
+    error = document.getElementById('error');
 
-function init() {
- 
- // Checking browser for WebAudio Support
- try {
-    // Fix up prefixing
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    audioContext = new AudioContext();
-  }
-  catch(e) {
-    alert("Web Audio API is not supported in this browser");
-  }  
-
-// Asynchronously load click sound from server
-  loadClick = new BufferLoader(audioContext, [url], 
-    function(bufferList){click = bufferList[0];});
-  loadClick.load();
-
-}
-
-function play() {
-
-  isPlaying = !isPlaying;
-
-  if ( isPlaying ) {
-    current4Note = 0;
-    nextNoteTime = audioContext.currentTime;
-    scheduler();
-    return "stop";
+playButton.onclick = function() {playButton.innerHTML = play();};
+bpm.oninput = function() {
+  if (bpm.value > 240 || bpm.value < 40) {
+  	error.innerHTML = "Bpm має бути не більше 240 та не меньше 40";
   } else {
-    window.clearTimeout( timerID );
-    return "play";
+  	range.value = bpm.value;
   }
-
-}
-
-function scheduler() {
-
-  //if (nextNoteTime < ) {
-    for (var bar = 0; bar < 10; bar++) {
-      var time = nextNoteTime + bar * tempo;
-      scheduleNote(time);  
-    }
-    nextNoteTime = time + 0.5;
-  //}
-  // while ( nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
-  //       scheduleNote( nextNoteTime );
-  //       nextNote();
-  //   }
-     timerID = window.setTimeout( scheduler, lookahead );
-
-}
-
-function scheduleNote(time) {
-
-  source = audioContext.createBufferSource();
-  source.buffer = click;
-  source.connect(audioContext.destination);
-  console.log(time);
-  source.start(time);
-
-  //source.stop( time + 0.05); 
-
-}
-
-function nextNote() {
-
-  var secondsPerBeat = 60.0 / tempo;
-
-  nextNoteTime += secondsPerBeat;
-
-  current4Note++;
-
-  if (current4Note == 32) {
-        current4Note = 0;
-    }
-
-}
-
-// XML request for sound load
-// TODO: rewrite this to simple function!
-  function BufferLoader( context, urlList, callback ) {
-    this.context = context;
-    this.urlList = urlList;
-    this.onload = callback;
-    this.bufferList = new Array();
-    this.loadCount = 0;
-  }
-
-  BufferLoader.prototype.loadBuffer = function( url, index ) {
-    // Load buffer asynchronously
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.responseType = "arraybuffer";
-
-    var loader = this;
-
-    request.onload = function() {
-      // Asynchronously decode the audio file data in request.response
-      loader.context.decodeAudioData(
-        request.response,
-        function(buffer) {
-          if (!buffer) {
-            alert( 'error decoding file data: ' + url );
-            return;
-          }
-          loader.bufferList[index] = buffer;
-          if ( ++loader.loadCount == loader.urlList.length )
-            loader.onload( loader.bufferList );
-        },
-        function(error) {
-          console.error( 'decodeAudioData error', error );
-        }
-      );
-    }
-
-    request.onerror = function() {
-      alert('BufferLoader: XHR error');
-    }
-
-    request.send();
-  }
-
-  BufferLoader.prototype.load = function() {
-    for (var i = 0; i < this.urlList.length; ++i)
-    this.loadBuffer(this.urlList[i], i);
-  }
-
-//})();
+  
+};
+range.oninput = function() {bpm.value = range.value;};
